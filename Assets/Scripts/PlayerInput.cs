9af1199c402cc86edfe8d2fa56d935d0461b6198
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2017-12-02 05:21:32 +0200

    main scene, mock character, basic movement

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	private float _xAxis;
+	private float _yAxis;
+	private Vector3 _movement;
+	private Transform _t;
+
+	private const float SPEED = 4f;
+
+	private void Awake()
+	{
+		_xAxis = 0f;
+		_yAxis = 0f;
+		_t = transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_xAxis = Input.GetAxis("Horizontal");
+		_yAxis = Input.GetAxis("Vertical");
+
+		_movement = new Vector3(_xAxis, 0f, _yAxis).normalized * Time.deltaTime * SPEED;
+		_t.Translate(_movement);
+
+	}
+
+	private void Roll()
+	{
+		// roll forward, drag back
+	}
+}
