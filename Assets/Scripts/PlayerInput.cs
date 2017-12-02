e3fb96e7b417d15524583263ac157829f3a45850
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2017-12-02 05:57:54 +0200

    dashing mechanic

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,34 +1,69 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+	private bool _dash;
 	private float _xAxis;
 	private float _yAxis;
 	private Vector3 _movement;
 	private Transform _t;
 
 	private const float SPEED = 4f;
+	private const float DASH_SPEED = 20f;
+	private const float DASH_TIME = 0.5f;
+
+	public AnimationCurve _dashAnimaton;
 
 	private void Awake()
 	{
 		_xAxis = 0f;
 		_yAxis = 0f;
 		_t = transform;
+		_isDashing = false;
 	}
+
+	private bool _isDashing;
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (_isDashing) return;
+
+		_dash = Input.GetButton("Fire1");
 		_xAxis = Input.GetAxis("Horizontal");
 		_yAxis = Input.GetAxis("Vertical");
 
-		_movement = new Vector3(_xAxis, 0f, _yAxis).normalized * Time.deltaTime * SPEED;
-		_t.Translate(_movement);
+		if (_dash)
+		{
+			_isDashing = true;
+			StartCoroutine(Dash());
+		}
+		else
+		{
+			_movement = new Vector3(_xAxis, 0f, _yAxis).normalized * Time.deltaTime * SPEED;
+			_t.LookAt(_t.position + _movement);
+			_t.Translate(_movement, Space.World);
+		}
 
 	}
 
-	private void Roll()
+	private IEnumerator Dash()
 	{
-		// roll forward, drag back
+		// step #1: decelerated speed boost
+		
+		float currentDashTime = 0f;
+		while (currentDashTime < DASH_TIME)
+		{
+			float currentDashSpeed = _dashAnimaton.Evaluate(currentDashTime / DASH_TIME) * Time.deltaTime * DASH_SPEED;
+			_movement = _t.forward * currentDashSpeed;
+			_t.Translate(_movement, Space.World);
+
+			currentDashTime += Time.deltaTime;
+			yield return null;
+		}
+
+
+		_isDashing = false;
 	}
 }
